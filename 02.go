f5907b82ff978adbc01c01c27eeb992e00b068c5
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-11-04 20:30:58 +0530

    Update 02.go

--- 02.go
+++ 02.go
@@ -12,3 +12,24 @@ func main() {
     fmt.Println("🎉 Happy New Year! 🎉")
 }
 
+//Infinite Loops - Handle with Care
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    i := 1
+    for {
+        fmt.Printf("Looping forever... %d\n", i)
+        time.Sleep(1 * time.Second) // Pauses for a second
+        i++
+        
+        if i > 5 { // Escape condition
+            fmt.Println("Alright, enough looping. Breaking out! 🚪")
+            break
+        }
+    }
+}
