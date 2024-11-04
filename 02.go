480116a92bb62ee55cc859762e342c79bd900a86
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-11-04 20:30:16 +0530

    Add loops

--- 02.go
+++ 02.go
@@ -1,1 +1,14 @@
+//loops
+//for loop
+package main
+
+import "fmt"
+
+func main() {
+    countdown := 10
+    for i := countdown; i > 0; i-- {
+        fmt.Println(i)
+    }
+    fmt.Println("🎉 Happy New Year! 🎉")
+}
 
