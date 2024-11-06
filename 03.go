5dcbbdd2a3edab228183226420b884bab995400b
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-11-06 22:47:03 +0530

    Update 03.go

--- 03.go
+++ 03.go
@@ -1,1 +1,25 @@
+//Functions
+package main
 
+import "fmt"
+func greet() {
+    fmt.Println("Hello, 👋 Welcome to Go functions.")
+}
+
+func main() {
+    greet() // Call the function
+}
+
+//Functions with Parameters
+package main
+
+import "fmt"
+
+func greetUser(name string) {
+    fmt.Printf("Hello, %s! 🎉 Welcome to Go functions.\n", name)
+}
+
+func main() {
+    greetUser("Denish")  // Try changing the name
+    greetUser("Alex")
+}
