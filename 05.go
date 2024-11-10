4286218dabe7c2bd41db143ac91d472f44363ef4
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-11-10 22:31:49 +0530

    Update 05.go

--- 05.go
+++ 05.go
@@ -1,1 +1,19 @@
+package main
+
+import "fmt"
+
+// Define a struct called 'Person'
+type Person struct {
+    Name string
+    Age  int
+}
+
+func main() {
+    // Create a new Person
+    var p Person
+    p.Name = "Denish"
+    p.Age = 21
+    fmt.Println("Name:", p.Name)
+    fmt.Println("Age:", p.Age)
+}
 
