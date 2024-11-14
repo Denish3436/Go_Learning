f2463794146b1b56571154280e7b234fbac66ac6
Denish Tomar <152975472+Denish3436@users.noreply.github.com>
2024-11-14 22:21:43 +0530

    Update 05.go

--- 05.go
+++ 05.go
@@ -38,3 +38,23 @@ func main() {
     jsonData, _ := json.Marshal(person)
     fmt.Println("JSON:", string(jsonData))
 }
+// Tags for Struct Fields
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+)
+
+type Person struct {
+    Name  string `json:"name"`
+    Age   int    `json:"age"`
+    Email string `json:"email"`
+}
+
+func main() {
+    person := Person{Name: "Eve", Age: 32, Email: "eve@example.com"}
+
+    jsonData, _ := json.Marshal(person)
+    fmt.Println("JSON:", string(jsonData))
+}
